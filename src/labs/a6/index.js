e360c2dbb9a435864094fa1521303aa70831c42d
zuoyifang <zuo.yif@northeastern.edu>
2023-05-11 21:24:42 -0700

    a6 before tuiter

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,7 +1,14 @@
+import Classes from "./classes";
+import ConditionalOutPut from "./conditional-output";
+import Styles from "./styles";
+
 function Assignment6() {
     return (
         <div>
             <h1>Assignment 6</h1>
+            <ConditionalOutPut/>
+            <Styles/>
+            <Classes/>
         </div>
     );
 }
